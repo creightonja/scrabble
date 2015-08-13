818382ed0f15e7a59b606c0d57ae59a6f27f146b
Jason Creighton & Michael Chastain <student@epicodus.com>
2015-08-13 11:43:08 -0700

    adds silex app functionality with twig.  Full basic implementation of spec.

---
+++ app/app.php
@@ -0,0 +1,34 @@
+<?php
+    //Loading vendor and source files
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/ScrabbleValues.php";
+
+    //Calling Debugger
+    use Symfony\Component\Debug\Debug;
+    Debug::enable();
+
+    //Calling silex framework
+    $app = new Silex\Application();
+    $app['debug'] = true;
+
+    //Setting views path for silex
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    //Setting Home page
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('submit.html.twig',array('result', '0'));
+    });
+
+    //Retrieving user input, setting instance, using ScrabbleValues method, then posting result.
+    $app->get("/view_result", function() use ($app) {
+        $scrabble_input = new ScrabbleValues;
+        $scrabble_value = $scrabble_input->getValues($_GET['scrabble_word']);
+        return $app['twig']->render('result.html.twig', array('result' => $scrabble_value);
+    });
+
+
+    return $app;
+?>
